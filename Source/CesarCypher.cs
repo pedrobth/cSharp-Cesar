7c041414c7f3c291ad1fe1bb142829ddb13b9d14
Pedro Risso <pmerchiorattor@gmail.com>
2021-06-16 20:55:20 -0300

    initial commit

---
+++ Source/CesarCypher.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Codenation.Challenge
+{
+    public class CesarCypher : ICrypt, IDecrypt
+    {
+        public char Encode (char l, int key = 3)
+        {
+    
+        }
+        
+        public string Crypt(string message)
+        {
+            
+        }
+
+        public string Decrypt(string cryptedMessage)
+        {
+          
+        }
+    }
+}
