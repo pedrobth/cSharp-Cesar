ba91c3f1d926fd1d465121dc1fec2b614f23bced
Pedro Risso <pmerchiorattor@gmail.com>
2021-06-16 21:05:17 -0300

    feat: cesar encrypt and decrypt

--- Source/CesarCypher.cs
+++ Source/CesarCypher.cs
@@ -7,17 +7,50 @@ namespace Codenation.Challenge
     {
         public char Encode (char l, int key = 3)
         {
-    
+            if(!char.IsLetter(l))
+            {
+                return l;
+            }
+            return (char)((((char.ToLower(l) + key) - 'a') % 26) + 'a');
         }
         
         public string Crypt(string message)
         {
-            
+            string codedMessage = string.Empty;
+            var acceptedCharsRegex = new Regex("^[a-zA-Z0-9 ]*$");
+            if (message == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!acceptedCharsRegex.IsMatch(message))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            foreach (char letter in message)
+            {
+                codedMessage += Encode(letter);
+            }
+            return codedMessage;
         }
 
         public string Decrypt(string cryptedMessage)
         {
-          
+          string decodedMessage = string.Empty;
+            int decryptKey = 26 - 3;
+            var acceptedCharsRegex = new Regex("^[a-zA-Z0-9 ]*$");
+            if (cryptedMessage == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!acceptedCharsRegex.IsMatch(cryptedMessage))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            foreach (char letter in cryptedMessage)
+            {
+                decodedMessage += Encode(letter, decryptKey);
+            }
+            return decodedMessage;
         }
     }
 }
